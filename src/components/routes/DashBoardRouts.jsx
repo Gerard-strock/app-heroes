56b4af017ebef6bc643805306e0c743919fb39a7
Gerardo Auyon <gera-1235@hotmail.com>
2022-05-19 21:42:41 -0500

    Add navigate and replace

--- src/components/routes/DashBoardRouts.jsx
+++ src/components/routes/DashBoardRouts.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import { DcScreen } from '../dc/DcScreen'
+import { HeroScreen } from '../hero/HeroScreen'
 import { MarvelScreen } from '../marvel/MarvelScreen'
 import { SearchScreen } from '../search/SearchScreen'
 import { Navbar } from '../ui/Navbar'
@@ -12,6 +13,7 @@ export const DashBoardRouts = () => {
         <Route path='/marvel' element={<MarvelScreen />} />
         <Route path='/dc' element={<DcScreen />} />
         <Route path='/search' element={<SearchScreen />} />
+        <Route path='/hero' element={<HeroScreen />} />
 
         <Route path='/' element={<MarvelScreen />} />
       </Routes>
