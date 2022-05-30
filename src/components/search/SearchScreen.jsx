4a8bacc11947ad5c2cf70f2423dd95ee44e52c89
Gerardo Auyon <gera-1235@hotmail.com>
2022-05-29 20:55:01 -0500

    Added component search

--- src/components/search/SearchScreen.jsx
+++ src/components/search/SearchScreen.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import { useMemo } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import queryString from 'query-string'
+
+import { getHeroesByName } from '../../helpers/getHeroesByName'
+import { useForm } from '../../hooks/useForm'
+import { HeroCard } from '../hero/HeroCard'
 
 export const SearchScreen = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const { q = '' } = queryString.parse(location.search)
+
+  const [formValues, handleInputChange] = useForm({
+    searchText: q
+  })
+
+  const { searchText } = formValues
+
+  const heroesFiltered = useMemo(() => getHeroesByName(q), [q])
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    navigate(`?q=${searchText}`)
+  }
+
   return (
     <div>
-      <h1>SearchScreen </h1>
+      <h1 style={{ textAlign: 'center' }}>SearchScreen </h1>
+      <hr />
+
+      <div className='row'>
+        <div className='col-5'>
+          <h4>Search For</h4>
+          <hr />
+
+          <form onSubmit={handleSearch}>
+            <input
+              type='text'
+              placeholder='Search a hero...'
+              className='form-control'
+              name='searchText'
+              autoComplete='off'
+              onChange={handleInputChange}
+              value={searchText}
+            />
+
+            <button
+              className='btn btn-outline-primary mt-1'
+            >
+              Search
+            </button>
+          </form>
+        </div>
+
+        <div className='col-7'>
+          <h4>Results</h4>
+          <hr />
+
+          {heroesFiltered.map((hero) => (
+            <HeroCard
+              key={hero.id}
+              {...hero}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -17,32 +17,25 @@ export const Navbar = () => {
 
       <div className='navbar-collapse'>
         <div className='navbar-nav'>
-          <NavLink
-            className='nav-item nav-link'
-            to='/marvel'
-          >
+          <NavLink className='nav-item nav-link' to='/marvel'>
             Marvel
           </NavLink>
 
           <NavLink className='nav-item nav-link' to='/dc'>
             DC
           </NavLink>
+
+          <NavLink className='nav-item nav-link' to='/search'>
+            Search
+          </NavLink>
         </div>
       </div>
 
       <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
         <ul className='navbar-nav ml-auto'>
+          <span className='nav-item nav-link text-info'>Gerardo</span>
 
-          <span
-            className='nav-item nav-link text-info'
-          >
-            Gerardo
-          </span>
-
-          <button
-            className='nav-item nav-link btn'
-            onClick={handleLogout}
-          >
+          <button className='nav-item nav-link btn' onClick={handleLogout}>
             Logout
           </button>
         </ul>
